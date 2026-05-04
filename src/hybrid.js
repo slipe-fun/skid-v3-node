41c76bc8cac2ce997e2b349178f9643760b36667
pip64 <mijak.iwisn@mail.ru>
2026-05-04 22:52:08 +0700

    add AAD binding to hybrid handshake for context integrity

--- src/hybrid.js
+++ src/hybrid.js
@@ -4,17 +4,28 @@ import { decrypt, encrypt } from './aes.js';
 import { hkdfExpand } from './hkdf.js';
 import { numberToBytes } from './utils.js';
 import { generateSalt } from './keys.js';
+import { buildAAD } from './aad.js';
 
-export function initiateKeyExchange(chat_id, sender_keys, receiver_keys) {
-    const { cipherText: senderCipherText, sharedSecret: senderPqcSharedSecret } = ml_kem512.encapsulate(sender_keys?.ml_kem?.public_key);
-    const { cipherText: receiverCipherText, sharedSecret: receiverPqcSharedSecret } = ml_kem512.encapsulate(receiver_keys?.ml_kem?.public_key);
+export function initiateKeyExchange(chat_id, sender, receiver) {
+    const { cipherText: senderCipherText, sharedSecret: senderPqcSharedSecret } = ml_kem512.encapsulate(sender?.ml_kem?.public_key);
+    const { cipherText: receiverCipherText, sharedSecret: receiverPqcSharedSecret } = ml_kem512.encapsulate(receiver?.ml_kem?.public_key);
 
-    const ecdhSharedSecret = x448.getSharedSecret(sender_keys?.ecdh?.secret_key, receiver_keys?.ecdh?.public_key);
+    const ecdhSharedSecret = x448.getSharedSecret(sender?.ecdh?.secret_key, receiver?.ecdh?.public_key);
 
     const syncMaterial = new Uint8Array([...senderPqcSharedSecret, ...ecdhSharedSecret]);
     const syncKey = hkdfExpand(syncMaterial, numberToBytes(chat_id), new TextEncoder().encode(`skid:v3:sync_key`), 32);
 
-    const encryptedSync = encrypt(syncKey, receiverPqcSharedSecret, generateSalt())
+    const nonce = generateSalt();
+
+    const aad = buildAAD("sync_material", {
+        chat_id,
+        sender_id: sender?.id,
+        receiver_id: receiver?.id,
+        senderCipherText,
+        receiverCipherText
+    });
+
+    const encryptedSync = encrypt(syncKey, receiverPqcSharedSecret, nonce, aad)
 
     const material = new Uint8Array([...receiverPqcSharedSecret, ...ecdhSharedSecret]);
 
@@ -28,20 +39,28 @@ export function initiateKeyExchange(chat_id, sender_keys, receiver_keys) {
     }
 }
 
-export function finalizeKeyExchange(chat_id, payload, sender_keys, receiver_keys, isSelf = false) {
+export function finalizeKeyExchange(chat_id, payload, sender, receiver, isSelf = false) {
     let pqcSharedSecret;
 
-    const ecdhSharedSecret = x448.getSharedSecret(receiver_keys?.ecdh?.secret_key, sender_keys?.ecdh?.public_key);
+    const ecdhSharedSecret = x448.getSharedSecret(receiver?.ecdh?.secret_key, sender?.ecdh?.public_key);
 
     if (isSelf) {
-        const pqcSharedSecret_A = ml_kem512.decapsulate(payload?.senderCipherText, sender_keys?.ml_kem?.secret_key);
+        const pqcSharedSecret_A = ml_kem512.decapsulate(payload?.senderCipherText, sender?.ml_kem?.secret_key);
 
         const syncMaterial = new Uint8Array([...pqcSharedSecret_A, ...ecdhSharedSecret]);
         const syncKey = hkdfExpand(syncMaterial, numberToBytes(chat_id), new TextEncoder().encode(`skid:v3:sync_key`), 32);
 
-        pqcSharedSecret = decrypt(syncKey, payload.encryptedSync.ciphertext, payload.encryptedSync.nonce);
+        const aad = buildAAD("sync_material", {
+            chat_id,
+            sender_id: sender?.id,
+            receiver_id: receiver?.id,
+            senderCipherText: payload?.senderCipherText,
+            receiverCipherText: payload?.receiverCipherText
+        })
+
+        pqcSharedSecret = decrypt(syncKey, payload.encryptedSync.ciphertext, payload.encryptedSync.nonce, aad);
     } else {
-        pqcSharedSecret = ml_kem512.decapsulate(payload?.receiverCipherText, receiver_keys?.ml_kem?.secret_key);
+        pqcSharedSecret = ml_kem512.decapsulate(payload?.receiverCipherText, receiver?.ml_kem?.secret_key);
     }
 
     const material = new Uint8Array([...pqcSharedSecret, ...ecdhSharedSecret]);
