dc48034b01f386f7cc57d9c4ba40eedca906aef1
pip64 <mijak.iwisn@mail.ru>
2026-05-18 18:12:12 +0700

    encrypted master key: fixed Uint8Array JSON stringification issue

--- protocol/master_key.js
+++ protocol/master_key.js
@@ -2,11 +2,12 @@ import { ed448 } from "@noble/curves/ed448.js";
 import { buildAAD } from "../src/aad.js";
 import { decrypt, encrypt } from "../src/aes.js";
 import { hkdfExpand } from "../src/hkdf.js";
-import { generateSalt } from "../src/keys.js";
+import { generateNonce, generateSalt } from "../src/keys.js";
+import { prepareForSigning } from "../src/utils.js";
 
 export function encryptMasterKey(master_key, recovery_key, secret_sign_key) {
     const salt = generateSalt();
-    const nonce = generateSalt();
+    const nonce = generateNonce();
 
     const derivedKey = hkdfExpand(recovery_key, salt, new TextEncoder().encode("skid:v3:recovery_key"), 32);
 
@@ -16,9 +17,10 @@ export function encryptMasterKey(master_key, recovery_key, secret_sign_key) {
 
     const encrypted = encrypt(derivedKey, master_key, nonce, aad);
 
-    const signature = ed448.sign(new TextEncoder().encode(JSON.stringify({
+    const signature = ed448.sign(new TextEncoder().encode(JSON.stringify(prepareForSigning({
         ...encrypted,
-    })), secret_sign_key)
+        salt
+    }))), secret_sign_key, { context: new Uint8Array(0) })
 
     return {
         ...encrypted,
@@ -34,10 +36,11 @@ export function decryptMasterKey(encrypted_master_key, recovery_key, public_sign
         nonce: encrypted_master_key?.nonce
     })
 
-    if (!ed448.verify(encrypted_master_key?.signature, new TextEncoder().encode(JSON.stringify({
+    if (!ed448.verify(encrypted_master_key?.signature, new TextEncoder().encode(JSON.stringify(prepareForSigning({
         ciphertext: encrypted_master_key?.ciphertext,
-        nonce: encrypted_master_key?.nonce
-    })), public_sign_key)) throw new Error("invalid signature");
+        nonce: encrypted_master_key?.nonce,
+        salt: encrypted_master_key?.salt
+    }))), public_sign_key, { context: new Uint8Array(0) })) throw new Error("invalid signature");
 
     return decrypt(derivedKey, encrypted_master_key?.ciphertext, encrypted_master_key?.nonce, aad)
 }
